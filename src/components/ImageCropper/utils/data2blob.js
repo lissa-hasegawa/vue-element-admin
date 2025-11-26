80cc1383e5969f0db03755342477a5ff17833547
AnaaGabs <g.ana@ufms.br>
2025-11-26 18:46:12 -0400

    refactor: Refatoração de comentários excessivos do código - resolver erro de Introduce Explaining Variable

--- src/components/ImageCropper/utils/data2blob.js
+++ src/components/ImageCropper/utils/data2blob.js
@@ -1,9 +1,8 @@
 /**
- * database64文件格式转换为2进制
- *
- * @param  {[String]} data dataURL 的格式为 “data:image/png;base64,****”,逗号之前都是一些说明性的文字，我们只需要逗号之后的就行了
- * @param  {[String]} mime [description]
- * @return {[blob]}      [description]
+ * dataURL
+ * @param  {[String]} data
+ * @param  {[String]} mime
+ * @return {[blob]}
  */
 export default function(data, mime) {
   data = data.split(',')[1]
@@ -12,7 +11,7 @@ export default function(data, mime) {
   for (var i = 0; i < data.length; i++) {
     ia[i] = data.charCodeAt(i)
   }
-  // canvas.toDataURL 返回的默认格式就是 image/png
+
   return new Blob([ia], {
     type: mime
   })
